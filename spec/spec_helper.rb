5395dcc826c3c84f3e064b587ae9bcfd9e50817a
Jason Rudolph <github@jasonrudolph.com>
2011-05-30 15:34:27 -0400

    Tell Spork to reload files in lib dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,8 +37,7 @@ Spork.prefork do
 end
 
 Spork.each_run do
-  # This code will be run each time you run your specs.
-
+  Dir["#{Rails.root}/lib/**/*.rb"].each { |f| load f }
 end
 
 # --- Instructions ---
