3aa4a89ee6ad3afdb0561b253db3a319c59d4c3f
Jason Rudolph <github@jasonrudolph.com>
2011-08-05 11:02:01 -0400

    Update stratify-rails to Rails 3.1.0.rc5
    Update stratify-rails to reflect the changes between
    Rails 3.1.0 RC 1 and RC5:
    
      - Update Gemfile to include the :assets group.  This
        matches the default Gemfile settings that you get when
        creating a brand new app with Rails 3.1.0.rc5.
      - Update config/application.rb to require the gems in the
        :assets group.  This matches the default application.rb
        settings that you get when creating a brand new app
        with Rails 3.1.0.rc5.
      - Update config/environments/* files to match the
        settings that you get when creating a brand new app
        with Rails 3.1.0.rc5.

--- stratify-rails/config/application.rb
+++ stratify-rails/config/application.rb
@@ -4,9 +4,10 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+# If you have a Gemfile, require the default gems, the ones in the
+# current environment and also include :assets gems if in development
+# or test environments.
+Bundler.require *Rails.groups(:assets) if defined?(Bundler)
 
 module Stratify
   class Application < Rails::Application
@@ -31,13 +32,6 @@ module Stratify
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # Please note that JavaScript expansions are *ignored altogether* if the asset
-    # pipeline is enabled (see config.assets.enabled below). Put your defaults in
-    # app/assets/javascripts/application.js in that case.
-    #
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(prototype prototype_ujs)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
