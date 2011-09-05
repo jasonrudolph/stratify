efe79035c41289481411bd14e34bb078113e23f9
Jason Rudolph <github@jasonrudolph.com>
2011-09-05 09:48:58 -0400

    Update stratify-rails to Rails 3.1.0
    Update stratify-rails to reflect the changes between
    Rails 3.1.0 RC 5 and the official 3.1.0 release:
    
      - Update versions in Gemfile
      - Update config/application.rb and config/environments/*
        files to match the settings that you get when creating
        a brand new app with Rails 3.1.0.

--- stratify-rails/config/application.rb
+++ stratify-rails/config/application.rb
@@ -4,10 +4,12 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 
-# If you have a Gemfile, require the default gems, the ones in the
-# current environment and also include :assets gems if in development
-# or test environments.
-Bundler.require *Rails.groups(:assets) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Stratify
   class Application < Rails::Application
@@ -41,5 +43,8 @@ module Stratify
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
