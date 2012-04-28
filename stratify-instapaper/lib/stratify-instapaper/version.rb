e2de11e095fd5c36ff168d50ca2d4ced121f466b
Jason Rudolph <github@jasonrudolph.com>
2012-04-28 17:46:28 -0400

    Bump version on all collector gems to 0.1.4

--- stratify-instapaper/lib/stratify-instapaper/version.rb
+++ stratify-instapaper/lib/stratify-instapaper/version.rb
@@ -1,5 +1,5 @@
 module Stratify
   module Instapaper
-    VERSION = "0.1.3.1"
+    VERSION = "0.1.4"
   end
 end
--- stratify-twitter/lib/stratify-twitter/version.rb
+++ stratify-twitter/lib/stratify-twitter/version.rb
@@ -1,5 +1,5 @@
 module Stratify
   module Twitter
-    VERSION = "0.1.3"
+    VERSION = "0.1.4"
   end
 end
