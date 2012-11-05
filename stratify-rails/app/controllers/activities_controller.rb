16c9ddba5922bc005b8fffe8a666fea130a6ea63
Jason Rudolph <github@jasonrudolph.com>
2012-11-04 20:17:53 -0500

    Improve pagination performance
    Since the latest version of Kaminari resolves issue #145, we can now
    paginate in a more natural fashion, and doing so greatly improves
    performance. The previous workaround required us to read all documents
    into an array and then paginate the array. (Yep: That's both janky and
    s-l-o-w.) Now we can paginate as intended (i.e., only fetch an
    individual page of documents from the database).
    
    Because ActivitiesController#index is currently the only place where we
    need pagination, I'm intentionally configuring the model's pagination
    support directly inside that controller. If we eventually need
    pagination support elsewhere, we'll revisit this decision.

--- stratify-rails/app/controllers/activities_controller.rb
+++ stratify-rails/app/controllers/activities_controller.rb
@@ -1,16 +1,20 @@
 class ActivitiesController < ApplicationController
   before_filter :prompt_user_to_setup_collectors_if_no_activities_exist, :only => :index
+  before_filter :enable_pagination_for_activities, :only => :index
 
   def index
-    # TODO Simplify the logic below once this bug is fixed in Kaminari:
-    #      https://github.com/amatsuda/kaminari/issues/145
-    activity_criteria = Stratify::Activity.desc(:created_at)
-    @activities = Kaminari.paginate_array(activity_criteria).page(params[:page]).per(200)
+    @activities = Stratify::Activity.desc(:created_at).page(params[:page]).per(200)
   end
 
   def destroy
     @activity = Stratify::Activity.find(params[:id])
     @activity.delete
     redirect_to activities_url
+  end
+
+  private
+
+  def enable_pagination_for_activities
+    Stratify::Activity.send :include, Kaminari::MongoidExtension::Document
   end
 end
