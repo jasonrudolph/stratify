2512debc05b687fd325a167c0f706fabec9c2cf6
Jason Rudolph <github@jasonrudolph.com>
2013-06-19 22:22:52 -0400

    Load core classes before loading any collectors

--- stratify-rails/config/initializers/collectors.rb
+++ stratify-rails/config/initializers/collectors.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
+require 'stratify/base'
+
 # Load each collector library
 Dir.glob(Rails.root.join('lib/stratify/*.rb')).each do |collector_lib|
   require collector_lib
